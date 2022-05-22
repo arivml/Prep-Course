be92924b31021f33049d61fac52185c68126a44d
arivml <arivml.1710@gmail.com>
2022-05-22 13:55:55 -0300

    una vez mas

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -128,7 +128,7 @@ function elevarAlCuadrado(num) {
   // Devuelve el valor de "num" elevado al cuadrado
   // ojo: No es raiz cuadrada!
   // Tu código:
-  return Math.pow(num, 2);
+  return Math.pow (num, 2);
 }
 
 function elevarAlCubo(num) {
