0aeed750ca411727f1797c3527613863e486979e
arivml <arivml.1710@gmail.com>
2022-06-17 21:44:12 -0300

    finished

--- 06-JS-V/homework/homework.js
+++ 06-JS-V/homework/homework.js
@@ -62,14 +62,14 @@ function agregarStringInvertida() {
       this.apellido = apellido,
       this.edad = edad,
       this.domicilio = domicilio
-      this.detalle = function() {
-        return { 
-          Nombre: this.nombre,
-          Apellido: this.apellido,
-          Edad: this.edad,
-          Domicilio: this.domicilio
-        }
-      }
+      // this.detalle = function() {
+      //   return { 
+      //     nombre: this.nombre,
+      //     apellido: this.apellido,
+      //     edad: this.edad,
+      //     domicilio: this.domicilio
+      //   }
+      // }
     }
   }
 
@@ -77,8 +77,8 @@ function crearInstanciaPersona(nombre, apellido, edad, dir) {
   //Con esta función vamos a crear una nueva persona a partir de nuestro constructor de persona (creado en el ejercicio anterior)
   //Recibirá los valores "Juan", "Perez", 22, "Saavedra 123" para sus respectivas propiedades
   //Devolver la nueva persona creada
-  const persona = new Persona(nombre, apellido, edad, dir);
-  return persona;
+  let person = new Persona('Juan', 'Perez', 22, 'Saavedra 123');
+  return Persona;
 }
   
 function agregarMetodo() {
